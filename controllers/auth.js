fe7562042fc2d231f7ef3683921b8dfbedddfc89
AkiP4 <67232776+AkiP4@users.noreply.github.com>
2021-03-30 19:07:57 +0300

    Added elements
    Added bcrypt module for password encryption. Finished up the user registration functions and noticed that users are being added to db properly.  Also added the function to detect duplicate emails from db and added the possibility to detect whether the passwords during registration don't match.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,52 @@
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+
+const db = mysql.createConnection({
+    host: process.env.database_host,
+    user: process.env.database_user,
+    password: process.env.database_password,
+    database: process.env.database
+});
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form submitted");
+
+    const { name, email, password, passwordConfirm } = req.body;
+
+    db.query("SELECT email FROM users WHERE email = ?", [email], async (error, results) => {
+        if(error) {
+            console.log(error);
+        }
+
+        if(results.length > 0 ) {
+            return res.render("register", {
+                message: "That email is already in use"
+            })
+
+        } else if(password !== passwordConfirm) {
+            return res.render("register", {
+            message: "Passwords do not match"
+        });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8); //usually 8 rounds of encryption is more than enough
+        console.log(hashedPassword);
+
+        db.query("INSERT INTO users SET ?", {name: name, email: email, password: hashedPassword }, (error, results) =>{
+            if(error) {
+                console.log(error);
+            } else {
+                console.log(results);
+                return res.render("register", {
+                    message: "User registered"
+                });
+            }
+        })
+
+    });
+
 }
+
+//npm i bcryptjs
+//npm i cookie-parser jsonwebtoken
