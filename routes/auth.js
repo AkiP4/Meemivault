c02296dd7d0da4817dc59c299e494a9524eafce9
AkiP4 <67232776+AkiP4@users.noreply.github.com>
2021-03-30 23:07:14 +0300

    Updated the page
    Updated the page and added files for the login function. The login works through cookies. I added cookie options and cookie expiration dates. I added the option for the code to check whether the user password or email is incorrect and return an appropriate result.
    
    As of right now, we don't have a logout button. After successfully logging in, the page just redirects you to the home page. This is where we start building the main page itself.
    
    ps. also some smaller changes were made to duplicate code sections.

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,9 @@ const authController = require("../controllers/auth")
 
 const router = express.Router();
 
-router.post("/register", authController.register )
+router.post("/register", authController.register );
+
+router.post("/login", authController.login);
 
 
 module.exports = router;
